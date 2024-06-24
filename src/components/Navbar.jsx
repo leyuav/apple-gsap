460ab63e11f3ffd640796a0c3de790f48826d924
leyuav <leyu.av@gmail.com>
2024-06-24 09:05:37 +0200

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,53 @@
-import { appleImg } from '../utils';
+import { appleImg, searchImg, bagImg } from '../utils';
+import { navLists } from '../constants';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { MdClose } from 'react-icons/md';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <header>
-      <nav>
-        <img src={appleImg} alt="" />
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="apple logo" width={14} height={18} />
+
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+            >
+              {nav}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex items-center gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="basket" width={18} height={18} />
+          <RxHamburgerMenu
+            className="text-white hidden max-sm:block"
+            fontSize={25}
+            onClick={() => setToggleMenu(true)}
+          />
+          {toggleMenu && (
+            <div className="w-full h-[100vh] flex flex-col items-center justify-center bg-black z-5 fixed top-0 left-0">
+              <MdClose
+                fontSize={30}
+                className="absolute top-4 right-4 cursor-pointer"
+                onClick={() => setToggleMenu(false)}
+              />
+              {navLists.map((nav) => (
+                <div
+                  key={nav}
+                  className="py-3 text-lg cursor-pointer text-gray hover:text-white transition-all"
+                >
+                  {nav}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </nav>
     </header>
   );
