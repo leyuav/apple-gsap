1d908bf9b30d989e1abcf2ecd2b211fa54c76b95
leyuav <leyu.av@gmail.com>
2024-06-26 22:11:21 +0200

    added lights and camera for models

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,20 @@
-const ModelView = () => {
+import { PerspectiveCamera, View } from '@react-three/drei';
+import Lights from './Lights';
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+    >
+      {/* ambient light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+    </View>
   );
 };
 
