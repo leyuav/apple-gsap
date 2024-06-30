872bb1e0254a9a9e09f7042cc7fd8a2a77230695
leyuav <leyu.av@gmail.com>
2024-06-30 16:32:03 +0200

    how it works section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import * as Sentry from '@sentry/react';
 import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
